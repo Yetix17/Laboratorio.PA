a58b1fd80cffb53c8e542db08118adae5f44ac7b
Test <thiagopirez01@gmail.com>
2025-03-26 13:44:55 -0300

    2da prueba

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     fecha date;
     date.dia=10, date.mes=20, date.anio=30;
     Informacion* info = new Informacion(12, date);
-//que andan manga de puto
+//que andan manga de putos todos putos profe enseña algo por dios
     cout << info->toString();
     return 0;
 }
