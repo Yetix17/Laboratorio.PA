7eb705c749da8b025f71a6ea8f655097e805833f
Lucho <luciano.digiovanni@estudiantes.utec.edu.uy>
2025-03-26 13:55:26 -0300

    a

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ int main() {
     fecha date;
     date.dia=10, date.mes=20, date.anio=30;
     Informacion* info = new Informacion(12, date);
-//que andan manga de putos todos putos
     cout << info->toString();
     return 0;
 }
