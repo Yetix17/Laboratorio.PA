a154b2e3935112e664e7495764c8add89b438a1d
Test <thiagopirez01@gmail.com>
2025-03-26 13:56:18 -0300

    3r

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main() {
     date.dia=10, date.mes=20, date.anio=30;
     Informacion* info = new Informacion(12, date);
 //que andan manga de putos todos putos profe enseña algo por
+
     cout << info->toString();
     return 0;
 }
